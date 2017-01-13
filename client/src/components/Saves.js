57f3a48fc006665da4a6a8185e1ac991013148ec
joehartz <jhartz01@gmail.com>
2017-01-12 20:35:00 -0500

    Added HTML entity support

--- client/src/components/Saves.js
+++ client/src/components/Saves.js
@@ -7,19 +7,29 @@ var _ = require('lodash');
 
 class Saves extends Component {
 
+  htmldecode(input){
+    var doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+  }
+
   render() {
     const filter = this.props.currentSubreddit !== 'all' ? ( _.filter(this.props.mylinks,['data.subreddit', this.props.currentSubreddit]) ) :
     (
       this.props.mylinks
     )
 
+
+
      const saves = filter.map((item) =>
            <div key={item.data.id} className="saveContainer">
            { item.data.thumbnail === 'default' || item.data.thumbnail === 'self' || item.data.thumbnail === '' ? '' : <div className="saveImg"><img className="imgthumb" alt="" src={item.data.thumbnail} /></div> }
-           <div className="saveContent">[+] <a href={item.data.url} target="_blank">{item.data.title ? item.data.title : item.data.link_title}</a></div>
+           <div className="saveContent"><a href={item.data.url ? item.data.url : item.data.link_url} target="_blank">{item.data.title ? item.data.title : item.data.link_title}</a></div>
+           <div className="top-description">{item.data.subreddit} | {item.data.domain} | {item.data.author ? item.data.author : item.data.link_author }</div>
+           <div dangerouslySetInnerHTML={{__html: this.htmldecode(item.data.body_html)}} />
+
          </div>
 
-         
+
 
 
      )
